5da7e5f6b438ea166bae8962861e1db36134a81f
Ben Newman <BenNewman@outlook.com>
2017-05-29 17:32:48 +0100

    Make indents consistent

--- jsfiddle.js
+++ jsfiddle.js
@@ -3,71 +3,71 @@ var fs = require('fs');
 var readline = require('readline');
 var exec = require('child_process').execSync;
 
-function getVersion(fiddleId, versionNumber){
-  console.log("Getting version " + versionNumber);
+function getVersion(fiddleId, versionNumber) {
+    console.log("Getting version " + versionNumber);
 
-  var requestOptions = {
-    host: 'fiddle.jshell.net',
-    port: 443,
-    path: '/' + fiddleId + '/' + versionNumber + '/show/light/',
-    headers: {
-      referer: 'https://fiddle.jshell.net/' + fiddleId + '/' + versionNumber + '/show/light/'
-    }
-  };
+    var requestOptions = {
+        host: 'fiddle.jshell.net',
+        port: 443,
+        path: '/' + fiddleId + '/' + versionNumber + '/show/light/',
+        headers: {
+            referer: 'https://fiddle.jshell.net/' + fiddleId + '/' + versionNumber + '/show/light/'
+        }
+    };
 
-  console.log("Starting request");
-  http.get(requestOptions, function(response){
-    switch(response.statusCode){
-      case 200:
-        console.log("ok");
-        response.on('data', function(chunk){
-          storeVersion(fiddleId, versionNumber, chunk.toString());
-        });
-        break;
-      case 404:
-        console.log("not found");
-        break;
-      default:
-        console.log(res);
-        console.log("err");
-        break;
-    }
-  }).end();
+    console.log("Starting request");
+    http.get(requestOptions, function (response) {
+        switch (response.statusCode) {
+            case 200:
+                console.log("ok");
+                response.on('data', function (chunk) {
+                    storeVersion(fiddleId, versionNumber, chunk.toString());
+                });
+                break;
+            case 404:
+                console.log("not found");
+                break;
+            default:
+                console.log(res);
+                console.log("err");
+                break;
+        }
+    }).end();
 }
 
-function storeVersion(fiddleId, version, response){
+function storeVersion(fiddleId, version, response) {
     var result = fs.writeFileSync(fiddleId + "/index.html", response);
     exec("git add .", { cwd: fiddleId });
 
     try {
-      exec("git commit -m \"Version " + version + "\"", { cwd: fiddleId });
+        exec("git commit -m \"Version " + version + "\"", { cwd: fiddleId });
     }
-    catch(err) {
-      console.warn("git commit failed (possibly no changes since last version?)");
+    catch (err) {
+        console.warn("git commit failed (possibly no changes since last version?)");
     }
-    
+
     fs.unlinkSync(fiddleId + '/index.html');
     getVersion(fiddleId, version += 1);
 }
 
-function initRepo(fiddleId){
-    if(fs.existsSync(fiddleId) == false){
-      fs.mkdir(fiddleId);
+function initRepo(fiddleId) {
+    if (fs.existsSync(fiddleId) == false) {
+        fs.mkdir(fiddleId);
     }
 
     exec("git init", { cwd: fiddleId });
 }
 
-function getFiddleId(){
-  var readLineInterface = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+function getFiddleId() {
+    var readLineInterface = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
 
-  readLineInterface.question('fiddle id: ', (answer) => {
-    initRepo(answer);
-    getVersion(answer, 0);
-  });
+    readLineInterface.question('fiddle id: ', (answer) => {
+        initRepo(answer);
+        getVersion(answer, 0);
+    });
 }
 
 getFiddleId();
